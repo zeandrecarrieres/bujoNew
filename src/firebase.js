00449f262f1fcd0754b7c922661d2077224adc9e
Jose Andre <zeandredev@gmail.com>
2026-03-29 01:01:17 +0100

    Security: Move Firebase credentials to .env

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCN9rkPgmkdSb7POBl_ned7nqhwBOiEwzM",
-  authDomain: "bujo-3a1aa.firebaseapp.com",
-  projectId: "bujo-3a1aa",
-  storageBucket: "bujo-3a1aa.firebasestorage.app",
-  messagingSenderId: "377247403948",
-  appId: "1:377247403948:web:efe5affa13e65c7681b409"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
